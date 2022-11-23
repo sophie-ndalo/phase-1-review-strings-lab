73dc4dc4d2ec774fe00d7810aa110168d91d86c9
sophia <sophiendalo@gmail.com>
2022-11-23 15:47:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code in this file!
+
+
+const currentUser = 'Sophie Ndalo';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let  excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+
+
+
+
